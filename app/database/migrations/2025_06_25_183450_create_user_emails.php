ece59282d2edde68361e5afc7a25ee0ec09168e4
Szymon Kostrubiec <github.sk99@gmail.com>
2025-06-25 20:57:28 +0200

    Update migrations

--- app/database/migrations/2025_06_25_183450_create_user_emails.php
+++ app/database/migrations/2025_06_25_183450_create_user_emails.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('user_emails', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('email');
             $table->timestamps();
         });
     }
